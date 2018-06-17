6797dbecb469bc171d9e7b99c3b4758dc17daaac
Fuji Nakahara <fujinakahara2032@gmail.com>
2018-06-17 16:49:33 +0900

    Create model `Term`

---
+++ db/migrate/20180617074445_create_terms.rb
@@ -0,0 +1,5 @@
+class CreateTerms < ActiveRecord::Migration[5.2]
+  def change
+    create_table :terms
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_06_17_074445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "terms", force: :cascade do |t|
+  end
 
 end
