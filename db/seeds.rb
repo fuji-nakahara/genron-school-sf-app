93f89398ec2d2d9a505eea30845caca569ecb33f
Fuji Nakahara <fujinakahara2032@gmail.com>
2018-06-30 00:24:43 +0900

    Improve seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+[2016, 2017, 2018].each do |id|
+  Term.create(id: id) unless Term.exists?(id: id)
+end
 
-Term.create([{ id: 2016 }, { id: 2017 }, { id: 2018 }])
+ScrapeStudentsJob.perform_now
+
+ScrapeSubjectsJob.perform_now
+
+Subject.all.each do |subject|
+  ScrapeSynopsesJob.perform_now(subject)
+  ScrapeWorksJob.perform_now(subject)
+end
