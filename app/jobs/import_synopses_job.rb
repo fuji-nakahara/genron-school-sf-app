afc3dcbbf3b639ef72447c70bdd2040ae0cec5b7
Fuji Nakahara <fujinakahara2032@gmail.com>
2019-01-12 18:33:20 +0900

    Update ImportSynopsesJob and ImportWorksJob

--- app/jobs/import_synopses_job.rb
+++ app/jobs/import_synopses_job.rb
@@ -7,12 +7,14 @@ class ImportSynopsesJob < ApplicationJob
     return if subject.no_synopsis?
     work_infos = GenronSf::Client.get_subject(subject.term_id, subject.number).synopses
     work_infos.each do |work_info|
-      work = GenronSf::Client.get_work(subject.term_id, work_info.student_id, work_info.id)
+      work    = GenronSf::Client.get_work(subject.term_id, work_info.student_id, work_info.id)
       student = Student.find_or_fetch_by!(original_id: work.student_id)
       Synopsis.find_or_create_by!(subject: subject, student: student, original_id: work.id) do |s|
-        s.title  = work.summary_title
-        s.body   = work.summary
-        s.appeal = work.appeal
+        s.title                  = work.summary_title
+        s.body                   = work.summary
+        s.character_count        = work.summary_character_count
+        s.appeal                 = work.appeal
+        s.appeal_character_count = work.appeal_character_count
       end
     end
   end
--- app/jobs/import_works_job.rb
+++ app/jobs/import_works_job.rb
@@ -6,12 +6,14 @@ class ImportWorksJob < ApplicationJob
   def perform(subject = Subject.previous)
     work_infos = GenronSf::Client.get_subject(subject.term_id, subject.number).works(no_synopsis: subject.no_synopsis?)
     work_infos.each do |work_info|
-      work = GenronSf::Client.get_work(subject.term_id, work_info.student_id, work_info.id)
+      work    = GenronSf::Client.get_work(subject.term_id, work_info.student_id, work_info.id)
       student = Student.find_or_fetch_by!(original_id: work.student_id)
       Work.find_or_create_by!(subject: subject, student: student, original_id: work.id) do |w|
-        w.title  = work.work_title
-        w.body   = work.body
-        w.appeal = work.appeal if subject.no_synopsis?
+        w.title                  = work.work_title
+        w.body                   = work.body
+        w.character_count        = work.work_character_count
+        w.appeal                 = work.appeal if subject.no_synopsis?
+        w.appeal_character_count = work.appeal_character_count if subject.no_synopsis?
       end
     end
   end
