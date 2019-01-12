76dcbc52bb1aa0bc23b20f64052bbc34e0779805
Fuji Nakahara <fujinakahara2032@gmail.com>
2019-01-13 01:29:46 +0900

    Remove pages /synopses/:id and /works/:id

--- app/controllers/synopses_controller.rb
+++
@@ -1,5 +0,0 @@
-class SynopsesController < ApplicationController
-  def show(id)
-    @synopsis = Synopsis.find(id)
-  end
-end
--- app/controllers/works_controller.rb
+++
@@ -1,5 +0,0 @@
-class WorksController < ApplicationController
-  def show(id)
-    @work = Work.find(id)
-  end
-end
